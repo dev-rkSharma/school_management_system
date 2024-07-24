557c6e1fe6016341cb985363f80152bd3a30b64b
rksharma <dev.rksharma1892@gmail.com>
2024-07-24 11:31:18 +0530

    nothing

--- pages/footer.php
+++ pages/footer.php
@@ -3,6 +3,8 @@
 
 <!-- scripts -->
 <!-- <script src="../scripts/jquery-3.5.1.min.js"></script> -->
+ <!-- bootstrap -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="../scripts/preloader.js"></script>
 <script src="../scripts/dashboard.js"> </script>
 <script src="../scripts/ajax.js"></script>
--- pages/registration_page.php
+++ pages/registration_page.php
@@ -1,30 +1,27 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            registration form
-        </title>
-        <link rel="stylesheet" href="../sass/registration/registration.css">
-    </head>
-    <body>
-        <main class='registration-form-container'>
-            <div class="heading">Student Registration From</div>
-            <form action="" id="registration-form" name="registration-form" method="post">
-                <div class="row">
-                    <section class="column">
-                        <fieldset>
-                            <legend>Student     Name </legend>
-                            <section class="input-section">
-                                <label for="fname">First : </label>
-                                <input type="text" name="fname" id="fname" placeholder="Enter your first name">
-                            </section>
-                            <section class="input-section">
-                                <label for="lname">Last : </label>
-                                <input type="text" id="lname:text" name="lname" placeholder="Enter last name">
-                            </section>
-                        </fieldset>
-                    </section>
-                </div>
-            </form>
-        </main>
+
+<head>
+    <title>
+        registration form
+    </title>
+    <!-- <link rel="stylesheet" href="../sass/registration/registration.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  
+    
+</head>
+
+<body>
+       
+        <!-- <main class='container'>
+        <div class="heading text-2xl">Student Registration From</div>
+        <form>
+           
+        </form>
+        </main> -->
+
+
+        
     </body>
+
 </html>
